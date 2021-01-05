898c3a82ba3f2bae82aca58f4bf9d41611288de1
M1-134-HamzaFaisal <76451177+M1-134-HamzaFaisal@users.noreply.github.com>
2021-01-05 20:58:34 +0300

    تم الحل

--- question1.js
+++ question1.js
@@ -1,1 +1,16 @@
 // قم بحل السؤال الأول هنا
+let ra=prompt(`whats your favorte pets?`);
+let pets =[ `cat`,`Kitten`, `Dog`,`Rabbit`,`GoldFish` ]
+console.log(pets)
+pets.pop();
+pets.push(ra);
+
+ 
+
+
+ let  organism ={
+    name: `Cat`,
+    age: `2 year`,
+    type: `Mammmal`,
+    emoji:`😻`}
+    console.log(organism.age);
